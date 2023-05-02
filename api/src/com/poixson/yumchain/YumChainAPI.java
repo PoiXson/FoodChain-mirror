bb9abb71ba159794a5462a2fa5e17b9cafafb42f
lorenzop <lorenzo@poixson.com>
2023-05-02 04:19:56 -0400

    added api files

---
+++ api/src/com/poixson/yumchain/YumChainAPI.java
@@ -0,0 +1,54 @@
+package com.poixson.yumchain;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.ServicesManager;
+
+
+public class YumChainAPI {
+	protected static final Logger LOG = Logger.getLogger("Minecraft");
+
+	protected static final String NAME  = "YumChain";
+	protected static final String CLASS = "com.poixson.yumchain.YumChainPlugin";
+
+	protected final YumChainPlugin plugin;
+
+	protected static final AtomicInteger errcount_PluginNotFound = new AtomicInteger(0);
+
+
+
+	public static YumChainAPI GetAPI() {
+		// existing instance
+		{
+			final ServicesManager services = Bukkit.getServicesManager();
+			final YumChainAPI api = services.load(YumChainAPI.class);
+			if (api != null)
+				return api;
+		}
+		// load api
+		try {
+			if (Class.forName(CLASS) == null)
+				throw new ClassNotFoundException(CLASS);
+			final PluginManager manager = Bukkit.getPluginManager();
+			final Plugin plugin = manager.getPlugin(NAME);
+			if (plugin == null) throw new RuntimeException(NAME+" plugin not found");
+			return new YumChainAPI(plugin);
+		} catch (ClassNotFoundException e) {
+			if (errcount_PluginNotFound.getAndIncrement() < 10)
+				LOG.severe("Plugin not found: "+NAME);
+			return null;
+		}
+	}
+
+	protected YumChainAPI(final Plugin p) {
+		if (p == null) throw new NullPointerException();
+		this.plugin = (YumChainPlugin) p;
+	}
+
+
+
+}

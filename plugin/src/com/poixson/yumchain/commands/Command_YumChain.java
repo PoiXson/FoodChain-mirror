733a906d11c1c1a58f7d9785907db1cc5db8703b
lorenzop <lorenzo@poixson.com>
2024-04-30 15:35:31 -0400

    updated pxnCommandRoot() construct

--- plugin/src/com/poixson/yumchain/commands/Command_YumChain.java
+++ plugin/src/com/poixson/yumchain/commands/Command_YumChain.java
@@ -15,10 +15,13 @@ public class Command_YumChain extends pxnCommandRoot {
 	public Command_YumChain(final YumChainPlugin plugin) {
 		super(
 			plugin,
+			"yumchain", // namespace
 			"I don't feel like eating this", // desc,
 			null, // usage
 			null, // perm
-			"yum", "yumchain", "yum-chain"
+			new String[] { // labels
+				"yum", "yumchain", "yum-chain"
+			}
 		);
 		this.cmd_list  = new Command_List(plugin);
 		this.cmd_reset = new Command_Reset(plugin);
